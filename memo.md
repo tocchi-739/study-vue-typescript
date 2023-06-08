fe7c995c66c402f9172a8e17c9b01cb1b4fa0626
tocchi <101105704+tocchi-739@users.noreply.github.com>
2023-06-08 23:58:19 +0900

    section4:34

---
+++ memo.md
@@ -0,0 +1,26 @@
+# memo
+
+- scoped
+
+  scoped を指定すると、そのファイルに指定した style は他のファイルに適用されない
+
+```
+<style scoped>
+</style>
+```
+
+- v-vind
+
+定義した変数を HTML 内で使える
+
+```
+<a v-bind:href="url">bought at...</a>
+```
+
+- v-on
+
+定義した関数を HTML 内で使える
+
+```
+<button v-on:click="buy">buy</button>
+```
---
+++ src/components/Payment.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const item1 = { name: "Desk", price: 300 };
+const item2 = { name: "Bike", price: 500 };
+
+const url = "https://www.yahoo.co.jp";
+
+const buy = (itemName: string) => {
+  alert(itemName + "を購入しますか？");
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>最近の支出</h1>
+    <div class="payment">
+      <label>{{ item1.name }}</label>
+      <label>{{ item1.price }}円</label>
+      <a v-bind:href="url">bought at...</a>
+      <button v-on:click="buy(item1.name)">buy</button>
+    </div>
+    <div class="payment">
+      <label>{{ item2.name }}</label>
+      <label>{{ item2.price }}円</label>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+.payment {
+  display: flex;
+  width: 400px;
+  align-items: center;
+  justify-content: space-between;
+  height: 80px;
+  margin-top: 60px;
+  background: rgb(189, 207, 244);
+  padding: 10px;
+}
+label {
+  font-size: 20px;
+}
+</style>
